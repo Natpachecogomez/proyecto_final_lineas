4a37d4ea198272ced228843fa33b1ddbefabb9ac
Natalia Pacheco Gómez <apachec93697@universidadean.edu.co>
2022-05-27 09:59:51 -0500

    Programa número cuánticos terminado

--- CE.py
+++ CE.py
@@ -1,20 +1,19 @@
-confele=[("1S",2),("2S",2),("2P",6),("3S",2),("3P",6),("4S",2),("3D",10),("4P",6),("5S",2),("4D",10),("5P",6),("6S",2),("4F",14),("5D",10),("6P",6),("7S",2),("5F",14),("6D",10),("7P",6)]
+#Lista configuración electrónica
+confele=[("1S",2),("2S",2),("2P",6),("3S",2),("3P",6),("4S",2),("3D",10),("4P",6),("5S",2),("4D",10),("5P",6),("6S",2),("4F",14),("5D",10),("6P",6),("7S",2),("5F",14),("6D",10),("7P",6),("6F",14),("7D",10),("7F",14)]
 
-
-#Pedir datos
-n=int(input("Ingrese valor de n: "))
-l=int(input("Ingrese valor de l: "))
-m=int(input("Ingrese valor de m: "))
-s=input("Ingrese si es positivo (+) o negativo(-): ")
+#Variables
 e=0
 
 #Rango de n
-if n>7 and n<1:
+n=int(input("Ingrese valor de n: "))
+if n>7 or n<1:
     print("El valor de n debe estar entre 1 y 7")
     n=int(input("Ingrese valor de n: "))
 
 #Rango de l y m, definir letras de l
-if l<0 and l>3:
+l=int(input("Ingrese valor de l: "))
+m=int(input("Ingrese valor de m: "))
+if l<0 or l>3:
     print("El valor de l debe estar entre 0 y 3")
     l=int(input("Ingrese valor de l: "))
 if l==0:
@@ -24,21 +23,22 @@ if l==0:
         m=int(input("Ingrese valor de m: "))
 elif l==1:
     l="P"
-    if m<-1 and m>1:
+    if m<-1 or m>1:
         print("El valor de m debe estar entre -1 y 1")
         m=int(input("Ingrese valor de m: "))
 elif l==2:
     l="D"
-    if m<-2 and m>2:
+    if m<-2 or m>2:
         print("El valor de m debe estar entre -2 y 2")
         m=int(input("Ingrese valor de m: "))
 else:
     l="F"
-    if m<-3 and m>3:
+    if m<-3 or m>3:
         print("El valor de m debe estar entre -3 y 3")
         m=int(input("Ingrese valor de m: "))
 
 #Rango de s
+s=input("Ingrese si es positivo (+) o negativo(-): ")
 if s!="+" and s!="-":
     print("El valor de s debe ser + o -")
     s=input("Ingrese si es positivo (+) o negativo(-): ")
@@ -132,9 +132,14 @@ if l=="F" and m==3:
     else:
         e=16   
 
+#Suma exponentes anteriores al que cambia
 sum=0
 for i in confele:
-    sum=int(i[2])+sum
-    if i[0]==str(n) and i[1]==l:
-        pass
-print(sum)
+    sum=i[1]+sum
+    if i[0][0]==str(n) and i[0][1]==l:
+        sum=sum-i[1]
+        break
+
+#Encotrar z
+z=sum+e
+print(z)
