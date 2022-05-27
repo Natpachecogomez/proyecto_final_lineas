1c9dcb26453fc49c706394843d62090945ecb86d
Natalia Pacheco Gómez <apachec93697@universidadean.edu.co>
2022-05-27 10:49:25 -0500

    Inicio programa elemento

--- CE.py
+++ CE.py
@@ -3,143 +3,152 @@ confele=[("1S",2),("2S",2),("2P",6),("3S",2),("3P",6),("4S",2),("3D",10),("4P",6
 
 #Variables
 e=0
+inicio=input("Para ingresar un elemento escriba e, para números cuánticos nc: ")
+if inicio!="e" and inicio!="nc":
+    input("Escriba e o nc: ")
 
-#Rango de n
-n=int(input("Ingrese valor de n: "))
-if n>7 or n<1:
-    print("El valor de n debe estar entre 1 y 7")
+#PROGRAMA PARA CUANDO INGRESA UN ELEMENTO
+if inicio==e:
+    algodiccionario=""
+
+#PROGRAMA PARA CUANDO INGRESA NÚMEROS CUÁNTICOS
+if inicio=="nc":
+    #Rango de n
     n=int(input("Ingrese valor de n: "))
+    if n>7 or n<1:
+        print("El valor de n debe estar entre 1 y 7")
+        n=int(input("Ingrese valor de n: "))
 
-#Rango de l y m, definir letras de l
-l=int(input("Ingrese valor de l: "))
-m=int(input("Ingrese valor de m: "))
-if l<0 or l>3:
-    print("El valor de l debe estar entre 0 y 3")
+    #Rango de l y m, definir letras de l
     l=int(input("Ingrese valor de l: "))
-if l==0:
-    l="S"
-    if m!=0:
-        print("El valor de m no puede ser diferente de 0")
-        m=int(input("Ingrese valor de m: "))
-elif l==1:
-    l="P"
-    if m<-1 or m>1:
-        print("El valor de m debe estar entre -1 y 1")
-        m=int(input("Ingrese valor de m: "))
-elif l==2:
-    l="D"
-    if m<-2 or m>2:
-        print("El valor de m debe estar entre -2 y 2")
-        m=int(input("Ingrese valor de m: "))
-else:
-    l="F"
-    if m<-3 or m>3:
-        print("El valor de m debe estar entre -3 y 3")
-        m=int(input("Ingrese valor de m: "))
+    if l<0 or l>3:
+        print("El valor de l debe estar entre 0 y 3")
+        l=int(input("Ingrese valor de l: "))
+    m=int(input("Ingrese valor de m: "))
+    if l==0:
+        l="S"
+        if m!=0:
+            print("El valor de m no puede ser diferente de 0")
+            m=int(input("Ingrese valor de m: "))
+    elif l==1:
+        l="P"
+        if m<-1 or m>1:
+            print("El valor de m debe estar entre -1 y 1")
+            m=int(input("Ingrese valor de m: "))
+    elif l==2:
+        l="D"
+        if m<-2 or m>2:
+            print("El valor de m debe estar entre -2 y 2")
+            m=int(input("Ingrese valor de m: "))
+    else:
+        l="F"
+        if m<-3 or m>3:
+            print("El valor de m debe estar entre -3 y 3")
+            m=int(input("Ingrese valor de m: "))
 
-#Rango de s
-s=input("Ingrese si es positivo (+) o negativo(-): ")
-if s!="+" and s!="-":
-    print("El valor de s debe ser + o -")
+    #Rango de s
     s=input("Ingrese si es positivo (+) o negativo(-): ")
+    if s!="+" and s!="-":
+        print("El valor de s debe ser + o -")
+        s=input("Ingrese si es positivo (+) o negativo(-): ")
 
-#Definir exponente que se suma para hallar z
-#S
-if l=="S" and m==0:
-    if s=="+":
-        e=1
-    else:
-        e=2
+    #Definir exponente que se suma para hallar z
+    #S
+    if l=="S" and m==0:
+        if s=="+":
+            e=1
+        else:
+            e=2
 
-#P
-if l=="P" and m==-1:
-    if s=="+":
-        e=1
-    else:
-        e=4
-if l=="P" and m==0:
-    if s=="+":
-        e=2
-    else:
-        e=5
-if l=="P" and m==1:
-    if s=="+":
-        e=3
-    else:
-        e=6
+    #P
+    if l=="P" and m==-1:
+        if s=="+":
+            e=1
+        else:
+            e=4
+    if l=="P" and m==0:
+        if s=="+":
+            e=2
+        else:
+            e=5
+    if l=="P" and m==1:
+        if s=="+":
+            e=3
+        else:
+            e=6
 
-#D
-if l=="D" and m==-2:
-    if s=="+":
-        e=1
-    else:
-        e=6
-if l=="D" and m==-1:
-    if s=="+":
-        e=2
-    else:
-        e=7
-if l=="D" and m==0:
-    if s=="+":
-        e=3
-    else:
-        e=8
-if l=="D" and m==1:
-    if s=="+":
-        e=4
-    else:
-        e=9
-if l=="D" and m==2:
-    if s=="+":
-        e=5
-    else:
-        e=10
+    #D
+    if l=="D" and m==-2:
+        if s=="+":
+            e=1
+        else:
+            e=6
+    if l=="D" and m==-1:
+        if s=="+":
+            e=2
+        else:
+            e=7
+    if l=="D" and m==0:
+        if s=="+":
+            e=3
+        else:
+            e=8
+    if l=="D" and m==1:
+        if s=="+":
+            e=4
+        else:
+            e=9
+    if l=="D" and m==2:
+        if s=="+":
+            e=5
+        else:
+            e=10
 
-#F
-if l=="F" and m==-3:
-    if s=="+":
-        e=1
-    else:
-        e=10
-if l=="F" and m==-2:
-    if s=="+":
-        e=2
-    else:
-        e=11
-if l=="F" and m==-1:
-    if s=="+":
-        e=3
-    else:
-        e=12
-if l=="F" and m==0:
-    if s=="+":
-        e=4
-    else:
-        e=13
-if l=="F" and m==1:
-    if s=="+":
-        e=5
-    else:
-        e=14
-if l=="F" and m==2:
-    if s=="+":
-        e=6
-    else:
-        e=15
-if l=="F" and m==3:
-    if s=="+":
-        e=7
-    else:
-        e=16   
+    #F
+    if l=="F" and m==-3:
+        if s=="+":
+            e=1
+        else:
+            e=10
+    if l=="F" and m==-2:
+        if s=="+":
+            e=2
+        else:
+            e=11
+    if l=="F" and m==-1:
+        if s=="+":
+            e=3
+        else:
+            e=12
+    if l=="F" and m==0:
+        if s=="+":
+            e=4
+        else:
+            e=13
+    if l=="F" and m==1:
+        if s=="+":
+            e=5
+        else:
+            e=14
+    if l=="F" and m==2:
+        if s=="+":
+            e=6
+        else:
+            e=15
+    if l=="F" and m==3:
+        if s=="+":
+            e=7
+        else:
+            e=16   
 
-#Suma exponentes anteriores al que cambia
-sum=0
-for i in confele:
-    sum=i[1]+sum
-    if i[0][0]==str(n) and i[0][1]==l:
-        sum=sum-i[1]
-        break
+    #Suma exponentes anteriores al que cambia
+    sum=0
+    for i in confele:
+        sum=i[1]+sum
+        if i[0][0]==str(n) and i[0][1]==l:
+            sum=sum-i[1]
+            break
 
-#Encotrar z
-z=sum+e
-print(z)
+    #Encotrar z
+    z=sum+e
+    print(z)
